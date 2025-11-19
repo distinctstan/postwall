ce1d3f25260ae6d8c3480dcf590b6e454f7869b2
Stanley <stan@Stanleys-MacBook-Pro.local>
2025-11-19 20:13:31 +0100

    Updated ALLOWED_HOSTS

--- postit/settings.py
+++ postit/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-85p&^gh*673w-mzi!d11=0lfky8khg7iv82ub-igbn5drfj)-x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://postwall-zvpv.onrender.com','127.0.0.1','localhost']
 
 
 # Application definition
